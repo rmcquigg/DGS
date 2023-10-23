610beffb2c2023593baa92f4ad8ac8cb2b651155
Rachel McQuiggan <55991096+rmcquigg@users.noreply.github.com>
2023-10-23 14:27:55 -0400

    Add files via upload

--- getData/DGS_getData.py
+++ getData/DGS_getData.py
@@ -7,34 +7,32 @@ Created on Wed May 17 17:24:28 2023
 
 import pandas as pd
 
+sites=['Ie55-07','Ie55-06','Ie444-05']
+
 def waterLevels(sites):
     dict={}
     with open('readme.txt', 'w') as f:
         f.write('Thanks for downloading water level data from the Delaware Geological Survey!'+
-                '\nPlease visit our website www.dgs.udel.edu for more information.'+
+                '\nPlease contact delgeosurvey@udel.edu with any questions.'+
                 '\nSITEID = DGS well identifier \DATE_MEASURED = Month/Day/Year'+
                 '\nTIME = HHMM Eastern Standard Time, DM indicates daily average taken from continuous logger-measurements'+
                 '\nTIMEZONE= Eastern Standard Time \nWATER_LEVEL = depth to water in feet below ground surface')
     for id in sites:
         url='http://data.dgs.udel.edu/sites/webwatlev/'+id+'.txt'
-        try: 
-            df=pd.read_csv(url,skiprows=3)
-            df=df.rename(columns=lambda x: x.strip())
-            df['SITEID']=id
-            df['TIMEZONE']='EST'
-            df['newt']=df['TIME'].where(df['TIME']!='DM','0000')
-            df['TIMESTAMP']=pd.to_datetime(df['DATE_MEASURED'] + " " + df['newt'])
-            df=df.drop('newt',axis=1)
-            cols=['SITEID','DATE_MEASURED','TIME','TIMEZONE','WATER_LEVEL','TIMESTAMP']
-            df=df[cols]
-            dict[id]=df.set_index('TIMESTAMP')
-            df.to_csv(id+'.csv',index='TIMESTAMP')
+        try: df=pd.read_csv(url,skiprows=3)
         except:
             print('Data for '+id+' are not available.')
             continue
-    return(dict)
-        
-        
+        df=df.rename(columns=lambda x: x.strip())
+        df['SITEID']=id
+        df['TIMEZONE']='EST'
+        df['newt']=df['TIME'].where(df['TIME']!='DM','0000')
+        df['TIMESTAMP']=pd.to_datetime(df['DATE_MEASURED'] + " " + df['newt'])
+        df=df.drop('newt',axis=1)
+        cols=['SITEID','DATE_MEASURED','TIME','TIMEZONE','WATER_LEVEL','TIMESTAMP']
+        df=df[cols]
+        dict[id]=df.set_index('TIMESTAMP')
+        df.to_csv(id+'.csv',index='TIMESTAMP')
         
     
 
